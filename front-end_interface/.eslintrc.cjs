eb4af01857a0ceb2ee095604acf7776b5ee35ff1
Ferdjani <ferdjani.abdelkaderzakaria@gmail.com>
2024-09-23 13:02:45 +0100

    Update ESLint configuration for stricter TypeScript and React linting

--- front-end_interface/.eslintrc.cjs
+++ front-end_interface/.eslintrc.cjs
@@ -1,18 +1,23 @@
 module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-  ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
+    root: true,
+    env: { browser: true, es2020: true },
+    extends: [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended-type-checked", // Stricter type checking
+        "plugin:react/recommended", // React linting rules
+        "plugin:react-hooks/recommended", // React Hooks linting
+        "plugin:react/jsx-runtime", // JSX runtime
     ],
-  },
-}
+    ignorePatterns: ["dist", ".eslintrc.cjs"],
+    parser: "@typescript-eslint/parser",
+    parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+        project: ["./tsconfig.json", "./tsconfig.node.json"],
+        tsconfigRootDir: __dirname,
+    },
+    plugins: ["react-refresh", "@typescript-eslint"],
+    rules: {
+        "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
+    },
+};
