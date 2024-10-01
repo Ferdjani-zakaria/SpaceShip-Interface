16ad8072b100d8d68e7eee00312483d03387d197
ferdjani-zakaria <ferdjani.abdelkaderzakaria@gmail.com>
2024-10-01 14:17:10 +0100

    added interface for markets

---
+++ front-end_interface/src/interfaces/playerInfo/marketInterface.ts
@@ -0,0 +1,37 @@
+export default interface Market {
+    data: {
+      symbol: string;
+      exports: TradeGood[];
+      imports: TradeGood[];
+      exchange: TradeGood[];
+      transactions?: Transaction[];
+      tradeGoods?: TradeGoodDetail[];
+    };
+  }
+  
+  interface TradeGood {
+    symbol: string;
+    name: string;
+    description: string;
+  }
+  
+  interface Transaction {
+    waypointSymbol: string;
+    shipSymbol: string;
+    tradeSymbol: string;
+    type: "PURCHASE" | "SELL";
+    units: number;
+    pricePerUnit: number;
+    totalPrice: number;
+    timestamp: string; // ISO date string
+  }
+  
+  interface TradeGoodDetail {
+    symbol: string;
+    type: "EXPORT" | "IMPORT" | "EXCHANGE";
+    tradeVolume: number;
+    supply: "SCARCE" | "PLENTIFUL";
+    activity: "WEAK" | "STRONG";
+    purchasePrice: number;
+    sellPrice: number;
+  }
