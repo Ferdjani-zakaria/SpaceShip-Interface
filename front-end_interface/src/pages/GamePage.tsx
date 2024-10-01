6cd64fc89fbf11baf29f8f0239990d589da7d8b9
ferdjani-zakaria <ferdjani.abdelkaderzakaria@gmail.com>
2024-10-01 14:14:30 +0100

    added a link to page TableMarket

--- front-end_interface/src/pages/GamePage.tsx
+++ front-end_interface/src/pages/GamePage.tsx
@@ -1,12 +1,14 @@
-import { FC, useState } from "react";
+import { FC, useState, useEffect } from "react";
 import Sidebar from "@components/Sidebar";
 import Overview from "@components/Overview";
 import TableContracts from "@components/TableContracts";
 import TableFleet from "@components/TableFleet";
 import Systems from "@components/Systems";
+import TableMarket from "@components/TableMarket";
 
 const GamePage: FC = () => {
     const [subPage, setSubPage] = useState<string>("Overview");
+    const [isWideScreen, setIsWideScreen] = useState<boolean>(window.innerWidth > 1600);
     const renderSubPage = () => {
         switch (subPage) {
             case "Overview":
@@ -17,17 +19,32 @@ const GamePage: FC = () => {
                 return <TableFleet />;
             case "Systems":
                 return <Systems />;
+            case "Markets":
+                return <TableMarket />;
             // Add more cases for other components
             default:
                 return <Overview />;
         }
     };
+
+    useEffect(() => {
+        const handleResize = () => {
+            setIsWideScreen(window.innerWidth > 1600);
+        };
+
+        window.addEventListener("resize", handleResize);
+
+        return () => {
+            window.removeEventListener("resize", handleResize);
+        };
+    }, []);
     return (
-        <div style={{ display: "flex", height: "93vh" }}>
-            <Sidebar setSubPage={setSubPage} />
-            <div style={{ flex: 1, maxWidth: "80%", marginInline: "auto" }}>{renderSubPage()}</div>
+        <div style={{ display: "flex",flexDirection: isWideScreen ? "column" : "row",  height: "93vh"}}>
+            <Sidebar setSubPage={setSubPage} wideScreen={isWideScreen}/>
+            <div style={{ width: "min(80%, 1400px)",marginInline: "auto" }}>{renderSubPage()}</div>
         </div>
     );
 };
+
 
 export default GamePage;
